5205356f71e97e703602319deafc872e884d874c
Wylliam <wylliam.cheng@gmail.com>
2026-06-27 14:44:03 -0400

    Adding parameter to MLP class to take in hyperparameter testing

--- src/vcpi_ml/models/mlp.py
+++ src/vcpi_ml/models/mlp.py
@@ -28,7 +28,8 @@ class MLPModel():
     def __init__(
         self, 
         n_in: int = 2048, n_out: int = 12995, hidden: int = 512,
-        lr: float = 1e-3
+        lr: float = 1e-3,
+        weight_decay: float = 0.0
         ):
         self.device = torch.device(
             "cuda" if torch.cuda.is_available()
@@ -38,35 +39,33 @@ class MLPModel():
         self.model = MLP(n_in=n_in, hidden=hidden, n_out=n_out)
         self.model.to(self.device)
         
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr, weight_decay=weight_decay)
         self.loss_fn = nn.MSELoss()
 
 
-    def fit(self, X: np.ndarray, Y: pd.DataFrame, epoch: int = 500, batch: int | None = None):
+    def fit(self, X: np.ndarray, Y: pd.DataFrame, epoch: int = 500, batch: int = 256):
 
         X, Y = (
             torch.tensor(X, dtype=torch.float32, device=self.device), 
             torch.tensor(Y.to_numpy(), dtype=torch.float32, device=self.device)
         )
         n = X.shape[0]
+        self.history = []
 
         for ep in tqdm(range(epoch)):
             perm = torch.randperm(n, device=self.device)
-            if batch:
-                for start in range(0, n, batch):
-                    idx = perm[start: start + batch]
-                    self.optimizer.zero_grad()
-                    xb, yb = X[idx], Y[idx]
-                    pred = self.model(xb)
-                    loss = self.loss_fn(pred, yb)
-                    loss.backward()
-                    self.optimizer.step()
-            else:
+            epoch_loss, n_batch = 0.0, 0
+            for start in range(0, n, batch):
+                idx = perm[start: start + batch]
                 self.optimizer.zero_grad()
-                pred = self.model(X)
-                loss = self.loss_fn(pred, Y)
+                xb, yb = X[idx], Y[idx]
+                pred = self.model(xb)
+                loss = self.loss_fn(pred, yb)
+                epoch_loss += loss.item()
+                n_batch += 1
                 loss.backward()
                 self.optimizer.step()
+            self.history.append(epoch_loss / n_batch)
 
         return self
 
